a03365e5cad43a73336da7fae8fd8926f11012c8
Ludvig Blixö <ludvigblixo@hotmail.com>
2020-10-23 12:22:50 +0200

    Added a folder for Infrastructure and added a help class APIClient.

---
+++ Interaktiva20_4/Data/Infrastructure/ApiClient.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Interaktiva20_4.Data.Infrastructure
+{
+    public class ApiClient
+    {
+        public async Task<T> GetTAsync<T>(string endpoint)
+        {
+            //TODO: Fixa så att koden inte upprepas
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(endpoint, HttpCompletionOption.ResponseHeadersRead);
+                response.EnsureSuccessStatusCode();
+                var data = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<T>(data);
+                return result;
+            }
+        }
+        // Await task1, task2; 
+    }
+    
+}
